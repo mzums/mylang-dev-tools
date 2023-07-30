de11b672c3d65c8eee9ae5196d9732700cb38333
mzums <annsie035@gmail.com>
2023-07-30 22:08:22 +0200

    ep01 - lexer

--- myopl/basic.py
+++ myopl/basic.py
@@ -23,7 +23,7 @@ class Error():
         return result
     
 
-class IllegalCharError(Error): 
+class IllegalCharError(Error):
 
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
